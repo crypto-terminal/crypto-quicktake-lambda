04584607f273cfab5eacdd038ba0b6e4ab9756d6
Yumin Gui <yumindev@gmail.com>
2022-06-10 21:49:27 -0700

    fix: calculate values in server instead of client

--- functions/binance_us/index.js
+++ functions/binance_us/index.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const fetch = require("node-fetch");
+const Decimal = require("decimal.js");
 
 const headers = {
     "Access-Control-Allow-Origin": "*",
@@ -12,74 +13,93 @@ const BASE_URL = "https://api.binance.us";
 const ENDPOINTS = {
     USER_ACCOUNT: "/api/v3/account",
     USER_STATUS: "/wapi/v3/accountStatus.html",
-    COIN_PRICES: "/api/v3/ticker/price"
-  };
-  
+    COIN_PRICES: "/api/v3/ticker/price",
+};
 
 const getSignature = (API_SECRET, timestamp) => {
     return crypto
-    .createHmac('sha256', API_SECRET)
-    .update(new URLSearchParams({ timestamp }).toString())
-    .digest('hex')
-}
+        .createHmac("sha256", API_SECRET)
+        .update(new URLSearchParams({ timestamp }).toString())
+        .digest("hex");
+};
 
-const getQuery = (signature, timestamp) =>
-  new URLSearchParams({ timestamp, signature }).toString();
-
+const getQuery = (signature, timestamp) => new URLSearchParams({ timestamp, signature }).toString();
 
 exports.handler = async function (event, context) {
     if (event.httpMethod !== "POST") {
         return {
             statusCode: 400,
             headers,
-            body: JSON.stringify({ success: false ,message: "Method Not supported"}),
+            body: JSON.stringify({ success: false, message: "Method Not supported" }),
         };
     }
 
-    const payload = JSON.parse(event.body)
-    const { pair } = payload;
+    try {
+        const payload = JSON.parse(event.body);
+        const { pair } = payload;
 
-    if (!pair || !pair.apiKey || !pair.apiSecret || !pair.ex) {
-        return {
-            statusCode: 400,
-            headers,
-            body: JSON.stringify({ success: false, message: "Field missing!", }),
-        };
-    }
-
-    const timestamp = Date.now();
-    const API_KEY = pair.apiKey;
-    const API_SECRET = pair.apiSecret;
-    const signature = getSignature(API_SECRET, timestamp);
-    const query = getQuery(signature, timestamp);
-
-    const fetchAccountInfo = fetch(
-        `${BASE_URL}${ENDPOINTS.USER_ACCOUNT}?${query}`,
-        {
-        method: "GET",
-        headers: {
-            "X-MBX-APIKEY": API_KEY,
-        },
+        if (!pair || !pair.apiKey || !pair.apiSecret || !pair.ex) {
+            return {
+                statusCode: 400,
+                headers,
+                body: JSON.stringify({ success: false, message: "Field missing!" }),
+            };
         }
-    );
-    const fetchCoinPrices = fetch(`${BASE_URL}${ENDPOINTS.COIN_PRICES}`);
 
-    const [accountInfoResponse, coinPricesResponse] = await Promise.all([
-        fetchAccountInfo, 
-        fetchCoinPrices
-    ]);
-    const accountInfo= await accountInfoResponse.json();
-    const coinPrices = await coinPricesResponse.json();
-    
+        const timestamp = Date.now();
+        const API_KEY = pair.apiKey;
+        const API_SECRET = pair.apiSecret;
+        const signature = getSignature(API_SECRET, timestamp);
+        const query = getQuery(signature, timestamp);
 
+        const fetchAccountInfo = fetch(`${BASE_URL}${ENDPOINTS.USER_ACCOUNT}?${query}`, {
+            method: "GET",
+            headers: {
+                "X-MBX-APIKEY": API_KEY,
+            },
+        });
+        const fetchCoinPrices = fetch(`${BASE_URL}${ENDPOINTS.COIN_PRICES}`);
 
-    return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({ 
-            success: true, 
-            message: "Hello World!", 
-            data: { accountInfo, coinPrices }
-        }),
-    };
-}
+        const [accountInfoResponse, coinPricesResponse] = await Promise.all([
+            fetchAccountInfo,
+            fetchCoinPrices,
+        ]);
+        let accountInfo = await accountInfoResponse.json();
+        const coinPrices = await coinPricesResponse.json();
+
+        accountInfo = {
+            ...accountInfo,
+            balances: accountInfo.balances
+                .filter((balance) => parseInt(balance.free) > 0)
+                .map((balance) => {
+                    let price = coinPrices.find((p) => p.symbol === `${balance.asset}USD`)?.price;
+                    price = price || "0";
+                    return {
+                        ...balance,
+                        price,
+                        value: new Decimal(price).times(balance.free).toFixed(2),
+                    };
+                }),
+        };
+
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({
+                success: true,
+                message: "Hello World!",
+                data: { accountInfo },
+            }),
+        };
+    } catch (err) {
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({
+                success: false,
+                message: "error",
+                data: null,
+            }),
+        };
+    }
+};
